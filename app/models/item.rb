dba2e512ec8e3563861e3347522cc8607f4c0fea
30155 <awlc.v41st@gmail.com>
2021-09-17 14:41:59 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
 end

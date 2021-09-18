929b42ee5d99196876620408233413621948afd4
30155 <awlc.v41st@gmail.com>
2021-09-18 18:20:17 +0900

    FactoryBot(items)の記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
-  factory :item do
-    
+
+  factory :item do 
+    item_name { Faker::Commerce.product_name }
+    explanation { Faker::Lorem.sentence }
+    category_id { Faker::Number.between(from: 1, to: 10) }
+    condition_id { Faker::Number.between(from: 1, to: 6) }
+    delivery_fee_id { Faker::Number.between(from: 1, to: 2) }
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    days_to_ship_id { Faker::Number.between(from: 1, to: 3) }
+    price { Faker::Number.between(from: 300, to: 9999999) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end

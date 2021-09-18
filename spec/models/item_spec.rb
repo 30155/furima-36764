c01597d32bfa5b1ac293ff74adb498165bc18c27
30155 <awlc.v41st@gmail.com>
2021-09-18 21:04:37 +0900

    単体テスト(Itemモデル)微調整

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,78 +10,78 @@ RSpec.describe Item, type: :model do
       it 'image, item_name、explanation、category_id、condition_id、delivery_fee_id、prefecture_id、days_to_ship_id、priceが存在すれば登録できる' do
         expect(@item).to be_valid
       end
-      it 'priceが300以上であれば登録できる' do
+      it 'priceが300以上であれば出品できる' do
         @item.price = 300
         expect(@item).to be_valid
       end
-      it 'priceが9999999以下であれば登録できる' do
+      it 'priceが9,999,999以下であれば出品できる' do
         @item.price = 9_999_999
         expect(@item).to be_valid
       end
     end
 
     context '商品出品できない場合' do
-      it 'imageが空では登録できない' do
+      it 'imageが空では出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it 'item_nameが空では登録できない' do
+      it 'item_nameが空では出品できない' do
         @item.item_name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
-      it 'explanationが空では登録できない' do
+      it 'explanationが空では出品できない' do
         @item.explanation = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
-      it 'category_idが空では登録できない' do
+      it 'category_idが空では出品できない' do
         @item.category_id = '---'
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it 'condition_idが空では登録できない' do
+      it 'condition_idが空では出品できない' do
         @item.condition_id = '---'
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
-      it 'delivery_fee_idが空では登録できない' do
+      it 'delivery_fee_idが空では出品できない' do
         @item.delivery_fee_id = '---'
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
       end
-      it 'prefecture_idが空では登録できない' do
+      it 'prefecture_idが空では出品できない' do
         @item.prefecture_id = '---'
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it 'days_to_ship_idが空では登録できない' do
+      it 'days_to_ship_idが空では出品できない' do
         @item.days_to_ship_id = '---'
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
-      it 'priceが空では登録できない' do
+      it 'priceが空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it 'priceが300未満では登録できない' do
+      it 'priceが300未満では出品できない' do
         @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
-      it 'priceが9999999より大きいと登録できない' do
+      it 'priceが9,999,999より大きいと出品できない' do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
-      it 'priceに半角数字以外が含まれていると登録できない' do
+      it 'priceに半角数字以外が含まれていると出品できない' do
         @item.price = '３００'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
-      it 'ユーザーが紐付いていなければ投稿できない' do
+      it 'ユーザーが紐付いていなければ出品できない' do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
